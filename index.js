5ac46a77e4ea8279b0cbe064f26dc9e19fc42176
Mojtaba Darabi <mojtabadarabi22@gmail.com>
2021-09-03 10:18:33 -0700

    hello feture added

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-console.log('hello men...')
-console.log('dfdf')
+
+let name;
 function welcome(name) {
     console.log(name +'welcome in new world ...')
 }
-function welcome(name) {
+function change(name) {
     console.log(name +'welcome in new world ...')
 }
+document.querySelector('#name').addEventListener('change',(e)=>{
+    name = e.target.value
+})
+document.querySelector('#welcome').addEventListener('click',()=>{
+    if (name) {
+        document.querySelector('h1').innerHTML='welcome '+name+'to my world'
+        
+    }
+})
+
