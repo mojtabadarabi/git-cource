cbabe390212486ccadde83c978b09736e4a45b60
Mojtaba Darabi <mojtabadarabi22@gmail.com>
2021-09-03 10:04:13 -0700

    welcome function added

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log('hello men...')
+console.log('dfdf')
+function welcome(name) {
+    console.log(name +'welcome in new world ...')
+}
+function welcome(name) {
+    console.log(name +'welcome in new world ...')
+}
